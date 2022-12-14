f3d050aa805eca650e79ab967969fd710240d938
hamda <hamda.baashe44@gmail.com>
2022-12-14 20:07:53 +0000

    completed tip-calculator-react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+
+ 
+  const [bill,setBill] = useState(0)
+ 
+  
+   const [tip,setTip] = useState()
+  const [total,setTotal] = useState(bill + tip )
+
+  
+   
+  function tipCalc(){
+  
+   
+  if( bill >= 50 && bill <= 300 ){
+    setTip(0.15 * bill);
+    setTotal(bill + tip)
+
+  } else {
+    setTip(0.20 * bill);
+    setTotal(bill + tip)
+     
+  }};
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='main'>
+      <div className="container">
+        <h1>Tip Calculator</h1>
+        <input type='text' placeholder="Enter your bill" onChange={(e)=>{
+           setBill(+e.target.value)
+        
+        }}/>
+        
+        <button onClick={tipCalc}>Calculate</button>
+        <p>Your bill  was {bill}, the tip was {tip}, and the total value is </p>
+        <div className="total"> {total}</div>
+      </div>
+      
     </div>
   );
 }
